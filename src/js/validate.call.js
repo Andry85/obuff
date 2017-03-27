9f799dbb6224ff08a21cc7f10584d56194c76915
andryShapoval <andryShapoval@gmail.com>
2017-03-27 11:36:06 +0300

    26.03.2017

--- src/js/validate.call.js
+++ src/js/validate.call.js
@@ -16,9 +16,7 @@ $(document).ready(function(){
             },
             phone:{
                 required: true
-            }
-
-            
+            }  
         
        },
        
@@ -40,7 +38,79 @@ $(document).ready(function(){
             }    
         
        }
-        
+
+    
     });
+
+
+    $("#myaccount").validate({
+        
+       rules:{ 
+        
+            fio:{
+                required: true,
+                minlength: 4,
+                maxlength: 50,
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            phone:{
+                required: true
+            },
+            password:{
+                required: true,
+                minlength: 4,
+                maxlength: 50,
+            },
+            passwordRepeat:{
+                required: true,
+                minlength: 4,
+                maxlength: 50,
+            }
+
+        
+       },
+       
+       messages:{
+        
+            fio:{
+                required: "Это поле обязательно для заполнения",
+                minlength: "Логин должен быть минимум 4 символа",
+                maxlength: "Максимальное число символо - 50",
+            },
+            
+            email:{
+                required: "Это поле обязательно для заполнения",
+                email: "Пожалуйста, введите адрес электронной почты",
+            },
+
+            phone:{
+                required: "Это поле необходимо заполнить."
+            },
+
+            password:{
+                required: "Это поле обязательно для заполнения",
+                minlength: "Логин должен быть минимум 4 символа",
+                maxlength: "Максимальное число символо - 50",
+            },
+
+            passwordRepeat:{
+                required: "Это поле обязательно для заполнения",
+                minlength: "Логин должен быть минимум 4 символа",
+                maxlength: "Максимальное число символо - 50",
+            }
+
+        
+       }
+
+    
+    });
+
+
+
+
+    
 
 }); //end of ready
